623bdd198581f4b3f9c820c8aacfaa9b31368d83
Sumit Hingu <sphingu@pm.me>
2022-12-14 15:37:22 +0000

    integrated mantine ui lib all packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {Button, MantineProvider} from '@mantine/core'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Button>Hello Mantine</Button>
+    </MantineProvider>
   </React.StrictMode>,
 )
