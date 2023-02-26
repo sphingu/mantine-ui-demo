533e57b174aa9105e181056c03e7c0d419f16c3f
Sumit Hingu <sumit.hingu@getweave.com>
2023-02-26 15:57:15 +0530

    refactor: solve zustand warning

--- src/stores/requestStore.ts
+++ src/stores/requestStore.ts
@@ -1,4 +1,4 @@
-import create from 'zustand'
+import { create } from 'zustand'
 import { immer } from 'zustand/middleware/immer'
 import { devtools, persist } from 'zustand/middleware'
 import { IRequestStore } from '../types'
