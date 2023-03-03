6a385ca230a332fc4f08af58378bfea8bf813093
Sumit Hingu <sumit.hingu@getweave.com>
2023-03-03 19:09:57 +0530

    feat: temp

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,57 @@
 import './main.css'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app'
+// import App from './app'
+import { Center, createStyles } from '@mantine/core'
+
+const useStyles = createStyles((theme) => ({
+  bgImg: {
+    // filter: 'blur(1px)',
+    position: 'absolute',
+    top: 0,
+    left: -80,
+  },
+  miraImg: {
+    position: 'absolute',
+    top: '-120px',
+    left: 100,
+  },
+  // ref: https://in.pinterest.com/pin/1051801687951177624/
+  name: {
+    position: 'absolute',
+    top: 40,
+    left: 15,
+    margin: 0,
+    rotate: '310deg',
+    // color: '#171731',
+    color: '#c1ffba',
+    letterSpacing: 2,
+    // textShadow: '13px 5px 52px #db2f3b',
+    textShadow: '0 0 10px black',
+  },
+}))
+
+const App = () => {
+  const { classes } = useStyles()
+
+  return (
+    <Center>
+      {/* <img className={classes.bgImg} src="green-bg.webp" /> */}
+      {/* <img className={classes.bgImg} src="bga.jpeg" height="1200" /> */}
+      {/* <img className={classes.bgImg} src="bgb.jpeg" height="1100" /> */}
+      <img className={classes.bgImg} src="main.png" height="1100" />
+      <img
+        className={classes.miraImg}
+        src="/mira.png"
+        height="1500"
+        width="675"
+      />
+      <p className={classes.name} style={{ fontSize: '5rem' }}>
+        Mira
+      </p>
+    </Center>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
